5f49f912060168368f68b25d0c91d9d6596db89b
JosAn <91902354+JosAnGoCa@users.noreply.github.com>
2022-08-30 19:09:29 -0500

    Rename: 02.2-Clase_arreglo_trabajadores to 03-Empresa

--- 02.2-Clase_arreglo_trabajadores/python/main.py
+++
@@ -1,7 +0,0 @@
-from clase_nomina import Nomina
-
-nomina = Nomina("junio.dat", "Estructura de datos S.A de C.V.")
-
-nomina.calcularSueldo()
-print("-----------------------------------------------------")
-nomina.obtenerMaxMin()
