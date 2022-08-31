19f6fab284562d75baa693a0195be9cb73e82199
JosAn <91902354+JosAnGoCa@users.noreply.github.com>
2022-08-30 19:21:51 -0500

    rename: nominaADT to Nomina in main

--- 03-Empresa/java/src/Main.java
+++ 03-Empresa/java/src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        nominaADT nomina = new nominaADT("junio.dat", "Estructura de datos S.A. de C.V.");
+        nominaADT nomina = new Nomina("junio.dat", "Estructura de datos S.A. de C.V.");
         nomina.calcularSueldo();
         System.out.println("-----------------------------------------------------");
         nomina.obtenerMaxMin();
